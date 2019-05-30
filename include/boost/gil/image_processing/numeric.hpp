fd59d1831b4f1bafb08b06f0e0865adcac7b1f01
Olzhas Zhumabek <anonymous.from.applecity@gmail.com>
2019-05-30 02:34:08 +0000

    Refactor lanczos into separate header
    This commit moves the algorithm
    into its own header. It also provides
    templated interface now, utilizing
    a little bit of decltype and declval
    to resolve types for lambdas and stuff

---
+++ include/boost/gil/image_processing/numeric.hpp
@@ -0,0 +1,23 @@
+#include <cmath>
+
+namespace boost{
+namespace gil{
+double normalized_sinc(double x) {
+    constexpr double pi = 3.14;
+    return std::sin(x * pi) / (x * pi);
+}
+
+double lanczos(long int x, long int a) {
+    if (x == 0) {
+        return 1;
+    }
+    if (x > -a && x < a) {
+        return normalized_sinc(static_cast<double>(x)) 
+                               / normalized_sinc((static_cast<double>(x))
+                / static_cast<double>(a));
+    }
+
+    return 0;
+}
+}
+}
