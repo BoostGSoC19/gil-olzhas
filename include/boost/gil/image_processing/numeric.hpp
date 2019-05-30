cd979f178167defa05b5832df23b7cec1c232d88
Olzhas Zhumabek <anonymous.from.applecity@gmail.com>
2019-05-30 03:34:43 +0000

    Improve documentation for Lanczos
    This commit adds documentation which
    describes when to use the algorithm,
    a brief description of how it is
    supposed to work, and some caution
    on the quality of the output

--- include/boost/gil/image_processing/numeric.hpp
+++ include/boost/gil/image_processing/numeric.hpp
@@ -2,12 +2,31 @@
 
 namespace boost{
 namespace gil{
+
+/// \defgroup ImageProcessingMath
+/// \brief Math operations for IP algorithms
+/// 
+/// This is mostly handful of mathemtical
+/// operations that are required by other
+/// image processing algorithms
+
+/// \brief Normalized cardinal sine
+/// \ingroup ImageProcessingMath
+///
+/// normalized_sinc(x) = sin(pi * x) / (pi * x)
 double normalized_sinc(double x) 
 {
     constexpr double pi = 3.14;
     return std::sin(x * pi) / (x * pi);
 }
 
+/// \brief Lanczos response at point x
+/// \ingroup ImageProcessingMath
+///
+/// Lanczos response is defined as:
+/// x == 0: 1
+/// x > -a && x < a: 0
+/// otherwise: normalized_sinc(x) / normalized_sinc(x / a)
 double lanczos(long int x, long int a) 
 {
     if (x == 0) {
@@ -15,8 +34,8 @@ double lanczos(long int x, long int a)
     }
     if (x > -a && x < a) {
         return normalized_sinc(static_cast<double>(x)) 
-                               / normalized_sinc((static_cast<double>(x))
-                / static_cast<double>(a));
+               / normalized_sinc((static_cast<double>(x))
+                                  / static_cast<double>(a));
     }
 
     return 0;
