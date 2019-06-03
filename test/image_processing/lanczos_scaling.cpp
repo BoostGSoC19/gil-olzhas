41f90173faa3d9e390872d097a8dd4357d54cafe
Olzhas Zhumabek <anonymous.from.applecity@gmail.com>
2019-06-03 02:58:13 +0000

    Implement handful of sanity tests
    This commit introduces a couple of
    sanity tests such as black image
    scaling to black image, and lanczos
    response being 0 at x = 0

--- test/image_processing/lanczos_scaling.cpp
+++ test/image_processing/lanczos_scaling.cpp
@@ -6,21 +6,65 @@
 #include <boost/gil/io/write_view.hpp>
 #include <boost/gil/image.hpp>
 
+#include <boost/core/lightweight_test.hpp>
+
 
 #include <iostream>
 
-int main(int argc, char* argv[]) 
-{
-    namespace gil = boost::gil;
-    if (argc != 3) {
-        std::cerr << "usage: " << argv[0] << " <input-filename.png> <output-filename.png>\n";
-        throw std::invalid_argument("invalid cli arguments");
+namespace gil = boost::gil;
+
+bool are_equal(gil::rgb8_view_t expected, gil::rgb8_view_t actual) {
+    if (expected.dimensions() != actual.dimensions())
+        return false;
+
+    for (long int y = 0; y < expected.height(); ++y) 
+    {
+        for (long int x = 0; x < expected.width(); ++x) 
+        {
+            if (expected(x, y) != actual(x, y)) 
+            {
+                return false;
+            }
+        }
     }
 
-    gil::rgb8_image_t input;
-    gil::read_image(argv[1], input, boost::gil::image_read_settings<gil::png_tag>{});
+    return true;
+}
 
-    gil::rgb8_image_t output(input.width() / 2, input.height() / 2);
-    scale_lanczos(gil::view(input), gil::view(output), 5l);
-    gil::write_view(argv[2], gil::view(output), gil::image_write_info<gil::jpeg_tag>{});
+void test_lanczos_black_image() 
+{
+
+    const gil::point_t input_dimensions(20, 20);
+    const gil::point_t output_dimensions(input_dimensions.x / 2, input_dimensions.y / 2);
+    gil::rgb8_image_t image(input_dimensions, gil::rgb8_pixel_t(0, 0, 0), 0);
+    // fill with values other than 0
+    gil::rgb8_image_t output_image(
+        output_dimensions,
+        gil::rgb8_pixel_t(100, 100, 100),
+        0
+    );
+    gil::rgb8_image_t expected(
+        output_dimensions,
+        gil::rgb8_pixel_t(0, 0, 0),
+        0
+    );
+
+    auto view = gil::view(image);
+    auto output_view = gil::view(output_image);
+    auto expected_view = gil::view(expected);
+    gil::scale_lanczos(view, output_view, 5);
+    BOOST_TEST(are_equal(expected_view,output_view));
+}
+
+void test_lanczos_response_on_zero() 
+{
+    //random value for a
+    BOOST_TEST(gil::lanczos(0, 2) == 1);
+}
+
+int main() 
+{
+    test_lanczos_black_image();
+    test_lanczos_response_on_zero();
+    return boost::report_errors();
 }
